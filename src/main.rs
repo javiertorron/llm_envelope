890df3c3adb2a7c765442fc2434a9b98db941b63
Javier Torron <javier.torron.diaz@gmail.com>
2026-06-22 21:52:07 +0200

    feat: add model directory and update main to generate random tensor via candle

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use candle_core::{DType, Device, Tensor};
+use candle_core::{Device, Tensor};
 
 fn main() {
-    // Queremos crear un Tensor muy simple en la CPU (una matriz llena de ceros) de tamaño 3x3.
+    // Creamos un Tensor en la CPU de tamaño 3x3 lleno de números aleatorios.
+    // Usamos una distribución normal con media 0.0 y desviación estándar 1.0
     let device = Device::Cpu;
 
-    // Tensor::zeros requiere 3 argumentos en las versiones recientes de candle: (shape, DType, &device)
-    let tensor = Tensor::zeros((3, 3), DType::F32, &device).unwrap();
+    let tensor = Tensor::randn(0f32, 1f32, (3, 3), &device).unwrap();
     println!("{:?}", tensor);
 }
